2cd3e53d59e910cff8b7f0e4dff2ee86bda4bf7c
JarifHassan <hassanjarif@gmail.com>
2025-04-26 01:59:38 +1200

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,9 +4,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy as sp
 
-df = pd.read_csv('imports-85.csv')
+df = pd.read_csv('imports-85.csv', header=None)
 
-df = df.iloc[:,1:]
 
 print(df.head())
 
+headers = ["symboling", "normalized-losses", "make",
+           "fuel-type", "aspiration","num-of-doors",
+           "body-style","drive-wheels", "engine-location",
+           "wheel-base","length", "width","height", "curb-weight",
+           "engine-type","num-of-cylinders", "engine-size",
+           "fuel-system","bore","stroke", "compression-ratio",
+           "horsepower", "peak-rpm","city-mpg","highway-mpg","price"]
+
+df.columns=headers
+print(df.head())
+
+
+data = df
+
+data.isna().any()
+data.isnull().any()
+
+data['city-mpg'] = 235 / df['city-mpg']
+data.rename(columns = {'city_mpg': "city-L/100km"}, inplace = True)
+print(data.columns)
+data.dtypes
