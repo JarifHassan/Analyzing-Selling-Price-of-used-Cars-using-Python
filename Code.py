b6dcff11b17fdbae1127510e5e5c2315f8807180
JarifHassan <hassanjarif@gmail.com>
2025-04-26 02:10:22 +1200

    Update Code.py

--- Code.py
+++ Code.py
@@ -79,3 +79,9 @@ plt.ylabel('Price')
 plt.grid()
 plt.show()
 
+test = data[['drive-wheels', 'body-style', 'price']]
+data_grp = test.groupby(['drive-wheels', 'body-style'],
+                        as_index = False).mean()
+data_grp
+
+
